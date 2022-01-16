48db641860c6ca4a3e4652d11bb75abc253177c4
Federico Rodriguez <solosilencio@gmail.com>
2022-01-16 21:53:47 +0100

    First commit

---
+++ precache-manifest.e39e48c1dd1c18b91d175912f2de9fd7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9001b72ee67f253dacfd38f8d9d1d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89ec864c3d4d14632ed0",
+    "url": "/static/css/main.3b699e3e.chunk.css"
+  },
+  {
+    "revision": "a18bedaf82ba49290b9d",
+    "url": "/static/js/2.554fd264.chunk.js"
+  },
+  {
+    "revision": "9e899dfd7ae2e062da19e83984708bc5",
+    "url": "/static/js/2.554fd264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ec864c3d4d14632ed0",
+    "url": "/static/js/main.e86f02b7.chunk.js"
+  },
+  {
+    "revision": "6477d806513b7364d23e",
+    "url": "/static/js/runtime-main.a789c4c1.js"
+  },
+  {
+    "revision": "217a27b0e79c9ef3c3ce9091db96ee77",
+    "url": "/static/media/Federico-Matias-Rodriguez.217a27b0.pdf"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "2fe2efe63441d830b1acd106c1fe8734",
+    "url": "/static/media/Simple-Line-Icons.2fe2efe6.svg"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/static/media/Simple-Line-Icons.f33df365.eot"
+  }
+]);
